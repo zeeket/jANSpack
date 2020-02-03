3c141646eab4307ffc4a862bad3c3fd6e85c49e1
zeeket <3qvm@tutanota.com>
2020-02-03 16:36:47 +0200

    cfa

--- jANSpack/src/main/java/jANSpack/RANS.java
+++ jANSpack/src/main/java/jANSpack/RANS.java
@@ -1,42 +1,47 @@
 package jANSpack;
 
- import java.lang.Math;
- import java.io.*; 
+import java.lang.Math;
+import java.io.*;
 
 public class RANS {
-    
+
     /**
      * @param inputByteArray
      * @param verbose
-     * @return 
+     * @return
      */
     public int[] decodingTable;
-    
+
     public RANS(byte[] inputByteArray, boolean verbose) {
         long key = 1;
         int[] CFAResult = new int[256];
-        for(int i=0;i<inputByteArray.length;i++){
-            int state = i;
-                    if(verbose){
-                        System.out.print("Reading byte "+i+ " of "+inputByteArray.length+" which is ");
-                        System.out.print((char) inputByteArray[i]+"\n");
-                        System.out.println("key is "+key);
-                        
-                    }
-                    int accumulator= inputByteArray[i] + 128;
-                    CFAResult[accumulator]++;
-                    
-                    
+        for (int i = 0; i < inputByteArray.length; i++) {
+
+            if (verbose) {
+                System.out.print("Reading byte " + i + " of " + inputByteArray.length + " which is ");
+                System.out.print(inputByteArray[i] + 128 + "\n");
+            }
+            int accumulator = inputByteArray[i] + 128;
+            CFAResult[accumulator]++;
+            if (verbose) {
+                System.out.println("So far we've read the byte called \"" + (inputByteArray[i] + 128) + "\" " + CFAResult[accumulator] + " times.");
+            }
         }
-        if(verbose){
-        System.out.println("");
+        if (verbose) {
+            System.out.println("=========\nCumulative frquency analysis complete\n==========");
+
+            for (int i = 0; i < CFAResult.length; i++) {
+
+                System.out.println("byte " + i + " was seen " + CFAResult[i] + " times.");
+
+            }
         }
         decodingTable = new int[10];
-        
+
     }
-    
-    public Pack getPack(){
-        return new Pack(12345,decodingTable);
+
+    public Pack getPack() {
+        return new Pack(12345, decodingTable);
     }
-    
+
 }
